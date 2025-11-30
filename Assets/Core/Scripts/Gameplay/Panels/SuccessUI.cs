2f3d43c2df19795f025567e08be1ded645474dd8
egekol <egekol@hotmail.com>
2025-11-30 10:37:16 +0300

    [Added] Star count

--- Assets/Core/Scripts/Gameplay/Panels/SuccessUI.cs
+++ Assets/Core/Scripts/Gameplay/Panels/SuccessUI.cs
@@ -1,3 +1,4 @@
+using Core.Scripts.Gameplay.Levels;
 using Core.Scripts.Gameplay.Managers;
 using Cysharp.Threading.Tasks;
 using DG.Tweening;
@@ -12,6 +13,10 @@ namespace Core.Scripts.Gameplay.Panels
         [SerializeField] private Button nextLevelButton;
         [SerializeField] private TextMeshProUGUI _energyCountTMP;
         [SerializeField] private CanvasGroup _canvasGroup;
+        [SerializeField] private Transform _starOne;
+        [SerializeField] private Transform _starTwo;
+        [SerializeField] private Transform _starThree;
+        
         
         private void OnEnable()
         {
@@ -30,9 +35,22 @@ namespace Core.Scripts.Gameplay.Panels
 
         public void Show()
         {
+            UpdateStars();
+            
             _canvasGroup.alpha = 0;
             gameObject.SetActive(true);
             _canvasGroup.DOFade(1f, 0.5f).SetLink(gameObject);
+        }
+
+        private void UpdateStars()
+        {
+            var levelModel = LevelManager.Instance.LevelModel;
+            var levelData = levelModel.LevelData;
+            var remainingMoves = levelModel.RemainingMoveCount;
+
+            _starOne.gameObject.SetActive(remainingMoves >= levelData.OneStarMinMoves);
+            _starTwo.gameObject.SetActive(remainingMoves >= levelData.TwoStarMinMoves);
+            _starThree.gameObject.SetActive(remainingMoves >= levelData.ThreeStarMinMoves);
         }
     }
 }
